871be2ebc8073ea15ff2279c9d95d8445bd05aa3
Akash06december <rbt23et015@gmail.com>
2026-07-27 17:56:10 +0530

    fix: add find operation

--- index.js
+++ index.js
@@ -21,44 +21,33 @@ const userSchema = new mongoose.Schema(
 const User = mongoose.model("User",userSchema); // add collections to the MongoDB
 const Employee = mongoose.model("Employee",userSchema);
 
-const user1 = new User({
-    name:"Eve",
-    email:"eve@yahoo.in",
-    age:48,
-});
+// const user3 = new User({
+//     name:"Swarup",
+//     age:21,
+//     email:"swarupP@gmail.com",
+//     tag: "lover"
+// });
 
-const user2 = new User({
-    name:"Parth",
-    email: "parth@gmail.com",
-    age:21
-})
-user1.save();
+// user3.save()
+// .then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
+// });
 
-user2.save().then((res)=>{
-    console.log(res);
-}).catch((err)=>{
-    console.log(err);
-});
+// First deleting the previous data and adding the new one for performing operations
 
-const user3 = new User({
-    name:"Swarup",
-    age:21,
-    email:"swarupP@gmail.com",
-    tag: "lover"
-});
+async function run() {
+    await User.deleteMany({});
 
-user3.save()
-.then((res)=>{
-    console.log(res);
-}).catch((err)=>{
-    console.log(err);
-});
+    await User.insertMany([
+        { name: "Priyanshu", email: "Pfifa@gmail.com", age: 21 },
+        { name: "Swayam", email: "stake@gmail.com", age: 32 },
+        { name: "Ganya", email: "GanyaC@gmail.com", age: 11 }
+    ]);
 
-User.insertMany([
-    {name:"Priyanshu",email:"Pfifa@gmail.com",age:21},
-    {name:"Swayam",email:"stake@gmail.com",age:22}
-]).then((res)=>{
-    console.log(res);
-}).catch((err)=>{
-    console.log(err);
-})
+    const res = await User.find({ age: { $gt: 20 } });
+    console.log(res[0].name);
+}
+
+run();
